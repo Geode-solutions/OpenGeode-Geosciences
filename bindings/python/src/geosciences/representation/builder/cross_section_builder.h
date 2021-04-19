1c42f4af5d9ba1727336b3d44fb5d3424ba9009c
Francois Bonneau <francois.bonneau@univ-lorraine.fr>
2021-04-19 14:17:05 +0200

    fix(Model): add copy in python binding

--- bindings/python/src/geosciences/representation/builder/cross_section_builder.h
+++ bindings/python/src/geosciences/representation/builder/cross_section_builder.h
@@ -35,6 +35,9 @@ namespace geode
             HorizonsBuilder2D, FaultBlocksBuilder2D,
             StratigraphicUnitsBuilder2D >( module, "CrossSectionBuilder" )
             .def( pybind11::init< CrossSection& >() )
+            .def( "copy", ( void ( CrossSectionBuilder::* )(
+                              const CrossSection& cross_section ) )
+                              & CrossSectionBuilder::copy )
             .def( "add_fault", ( const uuid& (CrossSectionBuilder::*) () )
                                    & CrossSectionBuilder::add_fault )
             .def( "add_horizon", ( const uuid& (CrossSectionBuilder::*) () )
--- bindings/python/src/geosciences/representation/builder/structural_model_builder.h
+++ bindings/python/src/geosciences/representation/builder/structural_model_builder.h
@@ -36,6 +36,9 @@ namespace geode
             HorizonsBuilder3D, FaultBlocksBuilder3D,
             StratigraphicUnitsBuilder3D >( module, "StructuralModelBuilder" )
             .def( pybind11::init< StructuralModel& >() )
+            .def( "copy", ( void ( StructuralModelBuilder::* )(
+                              const StructuralModel& structural_model ) )
+                              & StructuralModelBuilder::copy )
             .def( "add_fault", ( const uuid& (StructuralModelBuilder::*) () )
                                    & StructuralModelBuilder::add_fault )
             .def( "add_horizon", ( const uuid& (StructuralModelBuilder::*) () )
